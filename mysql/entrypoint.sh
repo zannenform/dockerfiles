e172b8af097cf859acaa2f92eaf619fb0ccf102d
maeda <logicle@sky.plala.or.jp>
2015-03-14 20:11:06 +0900

    mysql の Dockerfile修正

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 PASS="P@ssw0rd"
-if [ $# -ne 1 ]; then
+if [ $# -eq 1 ]; then
   PASS=$1
 fi
 
-
 echo "#1 Install MySQL:"
-DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
+echo mysql-server mysql-server/root_password password $PASS | debconf-set-selections
+echo mysql-server mysql-server/root_password_again password $PASS | debconf-set-selections
+apt-get install -y mysql-server
 apt-get clean
 
 echo "#2 Edit my.cnf:"
@@ -19,22 +20,6 @@ echo "skip-host-cache" >> /etc/mysql/my.cnf
 echo "skip-name-resolve" >> /etc/mysql/my.cnf
 
 echo "#3 Start MySQL:"
-/usr/bin/mysqld_safe &
-
-until $(mysqladmin ping > /dev/null 2>&1)
-do
-	sleep 3
-    echo ">"
-done
-
-echo "#4 MySQL config:"
-mysqladmin -u root password $PASS
-mysql -u root -p$PASS <<EOF
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$PASS' WITH GRANT OPTION;
-EOF
-
-echo "#5 Restarting mysql:"
-mysqladmin -p$PASS shutdown
 /usr/bin/mysqld_safe
 
-echo "#6 End:"
+echo "#4 End:"
